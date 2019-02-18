40c367a085c6dcca073832a4ebf13ff39476f395
Liam Lee <dltmdgud@nate.com>
2019-02-18 22:46:38 +0900

    add websocket, interval, timeout, immediate

--- SasRx.js
+++ SasRx.js
@@ -2,6 +2,7 @@ const REQUEST = require('request')
 const CSV = require('csvtojson')
 const RX = require('rx');
 const RSA = require('node-rsa');
+const WS_SERVER = require('websocket').server;
 
 function Following(observer, func, argcs) {
     const argc = argcs.shift();
@@ -14,6 +15,40 @@ function Following(observer, func, argcs) {
 }
 
 const ex = {
+    IntervalToObsv(ms, func) {
+        return RX.Observable.create(observer => {
+            const iserial = setInterval(() => {
+                observer.onNext(func());
+            }, ms);
+
+            return new RX.Disposable(() => {
+                clearInterval(iserial);
+            })
+        })
+    },
+    TimeoutToObsv(ms, func) {
+        return RX.Observable.create(observer => {
+            const tserial = setTimeout(() => {
+                observer.onNext(func());
+            }, ms);
+
+            return new RX.Disposable(() => {
+                clearTimeout(tserial);
+            })
+        })
+    },
+    ImmediateToObsv(func) {
+        return RX.Observable.create(observer => {
+            const iserial = setImmediate(() => {
+                observer.onNext(func());
+            });
+
+            return new RX.Disposable(() => {
+                clearImmediate(iserial);
+            })
+        })
+    },
+
     DownloadSheet(url) {
         return RX.Observable.create(observer => {
             CSV()
@@ -27,7 +62,6 @@ const ex = {
     },
 
     DecryptRsaKey(test, key) {
-
         return RX.Observable.fromPromise(new Promise((resolve, reject) => {
             let rsa = new RSA();
             rsa.importKey(key, 'pkcs1-private-pem');
@@ -36,8 +70,8 @@ const ex = {
         }));
     },
 
-    StreamToObserable: function(stream) {
-        return rx.Observable.create(obj => {
+    StreamToObsv: function(stream) {
+        return RX.Observable.create(obj => {
             stream
                 .on('error', (err) => {
                     obj.error(err);
@@ -51,9 +85,42 @@ const ex = {
         });
     },
 
-    FollowingAsObserverable(argcs, func) {
+    FollowingToObsv(argcs, func) {
         return RX.Observable.create(observer => {
             Following(observer, func, argcs);
+        });
+    },
+
+    WSListen(config) {
+        const ws = new WS_SERVER(config);
+        return RX.Observable.create(observer => {
+            ws.on('request', req => observer.onNext(req))
+                .on('connect', conn => observer.onNext(conn))
+                .on('close', () => observer.onCompleted());
+
+            return new RX.Disposable(() => {
+                ws.shutDown();
+                ws.removeAllListeners();
+            })
+        })
+    },
+
+    WSRecv(connection) {
+        return RX.Observable.create(observer => {
+            const onRecv = data => observer.onNext(data);
+            const onDisconn = () => observer.onCompleted();
+            const onError = err => observer.onError(err);
+            connection
+                .on('message', onRecv)
+                .on('close', onDisconn)
+                .on('error', onError)
+
+            return new RX.Disposable(() => {
+                connection
+                    .removeListener('message', onRecv)
+                    .removeListener('close', onDisconn)
+                    .removeListener('error', onError);
+            })
         });
     }
 }
